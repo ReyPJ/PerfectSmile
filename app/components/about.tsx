905851e616c5d397cd9daec80d180789d6496634
ReyPJ <reyner1012002@gmail.com>
2024-11-01 10:03:48 -0600

    prealfa

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -53,7 +53,7 @@ const AboutSection: React.FC = () => {
         {/* Descripción de equipo */}
         <p className="max-w-2xl text-center text-gray-600 mt-6">
           Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
           when an unknown printer took a galley of type and scrambled it to make a type
           specimen book.
         </p>
