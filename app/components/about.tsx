50d7b69ba5e2052c3d8b0f093a8dadce207ac324
ReyPJ <reyner1012002@gmail.com>
2024-11-29 09:54:21 -0600

    removing innecesary imports

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 const AboutSection: React.FC = () => {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import HomeSection from "./components/inicio";
 import EnHeader from "./components/en/enHeader";
 import AboutSection from "./components/about";
 import ServicesSection from "./components/services";
-import ReviewSection from "./components/reviews";
 import ContactSection from "./components/contact";
 import Header from "./components/header";
 import Footer from "./components/footer";
